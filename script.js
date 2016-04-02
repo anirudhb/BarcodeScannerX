8ca11efd4a4df149bb3ab67e60838923fd996498
anirudhb <anirudhqazxswced@gmail.com>
2016-04-02 14:49:17 -0700

    Price modification and bugfixes
    Added price modifier and fixed major bugs with UPC barcode length
    checking. Also nulls price if the price is not available but the item is
    found. Changed the close icon to a material icons and added the price
    modifier icon as a material icon, too. Future-proofed my alignment code.
    Italicized close icon and other icons. Removed padding on icons because
    it caused a problem with multiple items.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(function() {
     var key = String.fromCharCode(e.which);
     if (key == " " || key == "\n" || key == "\t" || key == "\r") {
       e.preventDefault();
-      if ($(".barcode-input").val().length > 12 || $(".barcode-input").val().length < 12) {
+      if ($(".barcode-input").val().length > 13 || $(".barcode-input").val().length < 11) {
         $(".barcode-input").val("");
         return;
       }
@@ -17,17 +17,50 @@ $(function() {
         success: function(data, status, fxhr) {
           var item = $(document.createElement("li"));
           var price = $(document.createElement("span"));
+          var closer = $(document.createElement("span"));
+          var priceMod = $(document.createElement("span"));
+          closer.text("clear");
+          closer.click(function() {
+            $(this).parent().remove();
+          });
+          priceMod.text("attach_money");
+          priceMod.click(function() {
+            var i = $(document.createElement("input"));
+            i.val($(this).parent().find(".price").clone().children().remove().end().text());
+            i.keydown(function(e) {
+              if (String.fromCharCode(e.which) == "\n" || String.fromCharCode(e.which) == "\r") {
+                e.preventDefault();
+                var price = $(this).parent().parent().find(".price");
+                price.text(Number(parseFloat($(this).val())).toFixed(2).toString());
+                // $(this).remove();
+              }
+            });
+            i.addClass("right");
+            $(this).parent().find(".price").html("");
+            $(this).parent().find(".price").append(i);
+            i.focus();
+          });
+          closer.addClass("right material-icons");
+          priceMod.addClass("right material-icons");
           price.addClass("price");
           item.addClass("item");
           if (data.items.length <= 0) {
             item.text("XXXXXXXXXX");
             price.text("0.00");
+            price.append(closer);
             item.append(price);
             $(".items").append(item);
             $(".barcode-input").val("");
           } else {
             item.text(data.items[0].title);
-            price.text(data.items[0]["lowest_recorded_price"]);
+            if (data.items[0]["lowest_recorded_price"] == undefined) {
+              price.text("0.00");
+            } else {
+              price.text(data.items[0]["lowest_recorded_price"].toFixed(2).toString());
+            }
+            closer.addClass("right");
+            item.append(priceMod);
+            item.append(closer);
             item.append(price);
             $(".items").append(item);
             $(".barcode-input").val("");
